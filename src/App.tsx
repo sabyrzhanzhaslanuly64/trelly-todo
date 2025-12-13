ecb74c99481ca0236ad3724fa2f3503da83e0373
Sabyrzhan Zhaslanuly <sabyrzhanzhaslanuly64@gmail.com>
2025-12-13 20:19:32 +0500

    feat: render algorithm based on data structures

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,68 @@
 const tasks = [
-  { id: 1, title: 'Купить продукты на неделю', isDone: false },
-  { id: 2, title: 'Полить цветы', isDone: true },
-  { id: 3, title: 'Сходить на тренировку', isDone: false },
+  {
+    id: 1,
+    title: 'Купить продукты на неделю',
+    isDone: false,
+    addedAt: '1 сентября',
+    priority: 2,
+  },
+  {
+    id: 2,
+    title: 'Полить цветы',
+    isDone: true,
+    addedAt: '2 сентября',
+    priority: 0,
+  },
+  {
+    id: 3,
+    title: 'Сходить на тренировку',
+    isDone: false,
+    addedAt: '3 сентября',
+    priority: 1,
+  },
+  {
+    id: 4,
+    title: 'Срочно отправить рабочий отчет',
+    isDone: false,
+    addedAt: '4 сентября',
+    priority: 4,
+  },
+  {
+    id: 5,
+    title: 'Заплатить за коммунальные услуги',
+    isDone: false,
+    addedAt: '3 сентября',
+    priority: 3,
+  },
 ]
 
+const priorities = ['#fff', '#ffd7b5', '#ffb38a', '#ff9248', '#ff6700']
+
 export const App = () => {
+  if (tasks === null) return <h1>Загрузка...</h1>
+
+  if (tasks.length === 0) return <h1>Задачи отсутствуют</h1>
+
   return (
-    <>
-      <h1>Список дел</h1>
-      <ul>
-        {tasks.map((task) => (
-          <li key={task.id}>
-            <div>{task.title}</div>
+    <ul>
+      {tasks.map((task) => (
+        <li key={task.id} style={{ background: priorities[task.priority] }}>
+          <p>
+            <b>Заголовок: </b>
+            <span style={{ textDecoration: task.isDone ? 'line-through' : 'none' }}>
+              {task.title}
+            </span>
+          </p>
+          <p>
+            <b>Статус: </b>
             <input type="checkbox" defaultChecked={task.isDone} />
-          </li>
-        ))}
-      </ul>
-    </>
+          </p>
+          <p>
+            <b>Дата создания задачи: </b>
+            <span>{task.addedAt}</span>
+          </p>
+        </li>
+      ))}
+    </ul>
   )
 }
