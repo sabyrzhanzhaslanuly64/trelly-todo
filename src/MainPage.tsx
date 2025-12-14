a658b6dded4695a7976fd6c2d9a6a77db465bb80
Sabyrzhan Zhaslanuly <sabyrzhanzhaslanuly64@gmail.com>
2025-12-14 15:32:59 +0500

    refactor: replace PageTitle div with h1 and set title text

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -2,11 +2,13 @@ import { Header } from './components/Header'
 import { TaskList } from './components/TaskList'
 import { TaskDetails } from './components/TaskDetails'
 import { Footer } from './components/Footer'
+import { PageTitle } from './components/PageTitle'
 
 export const MainPage = () => {
   return (
     <main>
       <Header />
+      <PageTitle />
       <div style={{ display: 'flex', gap: '30px' }}>
         <TaskList />
         <TaskDetails />
