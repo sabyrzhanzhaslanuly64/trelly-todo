7ee6129d4b9d1b9385382ec4e383b8236a6c8b73
Sabyrzhan Zhaslanuly <sabyrzhanzhaslanuly64@gmail.com>
2025-12-14 15:55:21 +0500

    chore: remove Header, PageTitle, and Footer from MainPage layout

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,19 +1,13 @@
-import { Header } from './components/Header'
-import { PageTitle } from './components/PageTitle'
 import { TaskList } from './components/TaskList'
 import { TaskDetails } from './components/TaskDetails'
-import { Footer } from './components/Footer'
 
 export const MainPage = () => {
   return (
     <main>
-      <Header />
-      <PageTitle />
       <div style={{ display: 'flex', gap: '30px' }}>
         <TaskList />
         <TaskDetails />
       </div>
-      <Footer />
     </main>
   )
 }
